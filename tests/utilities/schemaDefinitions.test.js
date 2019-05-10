d47df9ea8d36a785f63ebf5832d68fdd7d3a96e6
Ravikanth Dakarapu <ravikanth.dakarapu@gmail.com>
2019-05-10 12:17:43 +1000

    added schema validation unit tests

---
+++ tests/utilities/schemaDefinitions.test.js
@@ -0,0 +1,51 @@
+import * as Schemas from "../../src/utilities/schemaDefinitions";
+
+describe("Validation Schema Suit", () => {
+  // Reward Schema Validation
+  it("Test Successful Reward Validation", () => {
+    let res = Schemas.rewardObjValidation({ id: 100, name: "REWARD_1" });
+    expect(res).toBe(null);
+  });
+
+  it("Test Successful Reward Validation", () => {
+    let res = Schemas.rewardObjValidation({ id: 100, name: "RE" });
+    expect(res).toHaveProperty("name", "ValidationError");
+  });
+
+  // Member Schema Validation
+  it("Test Successful Reward Validation", () => {
+    let res = Schemas.memberObjValidation({ id: 500, name: "member_name" });
+    expect(res).toBe(null);
+  });
+
+  it("Test Successful Reward Validation", () => {
+    let res = Schemas.memberObjValidation({ name: "RE" });
+    expect(res).toHaveProperty("name", "ValidationError");
+    expect(res).toHaveProperty(
+      "message",
+      'child "id" fails because ["id" is required]'
+    );
+  });
+
+  // Member-Reward Schema Validation
+  it("Test Successful Reward Validation", () => {
+    let res = Schemas.memberRewardObjValidation({
+      memberId: 500,
+      rewardId: 100
+    });
+    expect(res).toBe(null);
+  });
+
+  it("Test Successful Reward Validation", () => {
+    let res = Schemas.memberRewardObjValidation({
+      memberId: 500,
+      rewardId: "REWARD_1"
+    });
+    expect(res).toHaveProperty("name", "ValidationError");
+
+    expect(res).toHaveProperty(
+      "message",
+      'child "rewardId" fails because ["rewardId" must be a number]'
+    );
+  });
+});
